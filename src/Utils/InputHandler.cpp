e32e3580b3b02cc2d1e0e60cc960e91743aeec67
SerhiyKoval <kovalss2006@gmail.com>
2025-11-20 22:17:25 +0200

    поправлена логіка чутливості

--- src/Utils/InputHandler.cpp
+++ src/Utils/InputHandler.cpp
@@ -34,31 +34,35 @@ void processInput(GLFWwindow* window) {
                        glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS;
 
     if (ctrlPressed) {
-        // Клавіші 1-3 з CTRL: чутливість повороту (миші)
+        if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS && !keysProcessed[2]) {
+            camera.setMouseSensitivity(0.01f);  
+            // std::cout << "Mouse sensitivity: 0.4" << std::endl;
+            keysProcessed[0] = true;
+        }
         if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS && !keysProcessed[0]) {
             camera.setMouseSensitivity(0.05f);  
             // std::cout << "Mouse sensitivity: 0.05" << std::endl;
-            keysProcessed[0] = true;
+            keysProcessed[1] = true;
         }
         if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS && !keysProcessed[1]) {
             camera.setMouseSensitivity(0.1f);   
             // std::cout << "Mouse sensitivity: 0.1" << std::endl;
-            keysProcessed[1] = true;
+            keysProcessed[2] = true;
         }
         if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS && !keysProcessed[2]) {
             camera.setMouseSensitivity(0.2f);  
             // std::cout << "Mouse sensitivity: 0.2" << std::endl;
-            keysProcessed[2] = true;
+            keysProcessed[3] = true;
         }
         if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS && !keysProcessed[2]) {
             camera.setMouseSensitivity(0.3f);  
             // std::cout << "Mouse sensitivity: 0.3" << std::endl;
-            keysProcessed[3] = true;
+            keysProcessed[4] = true;
         }
         if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS && !keysProcessed[2]) {
             camera.setMouseSensitivity(0.4f);  
             // std::cout << "Mouse sensitivity: 0.4" << std::endl;
-            keysProcessed[4] = true;
+            keysProcessed[5] = true;
         }
     } else {
         if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS && !keysProcessed[0]) {
